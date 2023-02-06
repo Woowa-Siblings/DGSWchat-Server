15c41aba822200ded28a28039fabd391f1f4b105
김종윤 <savagestrikekr@gmail.com>
2023-02-07 08:56:31 +0900

    init

---
+++ src/main/java/woowhahan/dgswchat/DgswchatApplication.java
@@ -0,0 +1,13 @@
+package woowhahan.dgswchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgswchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgswchatApplication.class, args);
+	}
+
+}
