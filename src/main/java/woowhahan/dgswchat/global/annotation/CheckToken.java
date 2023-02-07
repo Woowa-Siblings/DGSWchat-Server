70ccff3ce5962e49b2880b9fa844497c44fdcf68
김종윤 <savagestrikekr@gmail.com>
2023-02-07 20:51:41 +0900

    ADD: @CheckToken

---
+++ src/main/java/woowhahan/dgswchat/global/annotation/CheckToken.java
@@ -0,0 +1,9 @@
+package woowhahan.dgswchat.global.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface CheckToken {
+}
